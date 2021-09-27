8cb1a1a194501f89af1edb7dc2ea8821b73c9a02
Suman Gajmer <suman.gajmer@f1soft.com>
2021-09-27 23:14:40 +0545

    working mongoDB tutorial

---
+++ src/test/java/com/example/mangoDbTutrial/MangoDbTutrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mangoDbTutrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoDbTutrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
